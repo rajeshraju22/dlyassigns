30b83ace814878d990c6d05c88f068a143136c97
rajeshraju22 <rajeshraju.it22@gmail.com>
2016-08-11 21:33:34 +0530

    S5-a1
    session5-assignment1

---
+++ dlyassigns/assigns/calcCircleArea-s5-a1.js
@@ -0,0 +1,19 @@
+﻿function calcirclearea(r)
+{
+    var area = (3.14 * r * r);
+    document.write("r=" + r +"</br>");
+    document.write("area=" + area);
+}
+
+function calcriclearea(r)
+{
+    var area2 = (11.2 * r * r);
+    document.write("r=" + r);
+    document.write("area" + r);
+}
+function calcriclearea(r)
+{
+    var area3 = (20.2 * r * r);
+    document.write("r" + r + r);
+    document.write("area" + r);
+}
